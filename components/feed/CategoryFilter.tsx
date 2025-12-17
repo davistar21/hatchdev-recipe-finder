0883eff1f9cc2f7977b2ff2b2f761009b8ff3fbb
Mayor-king <maylaw203@gmail.com>
2025-12-17 10:34:41 +0100

    UPDATE: SearchBar and CategoryFilter to search and update filter

--- components/feed/CategoryFilter.tsx
+++ components/feed/CategoryFilter.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { useSearchStore } from "@/lib/stores/useSearchStore";
 
 const CATEGORIES = [
   { label: "All", active: true },
@@ -12,22 +13,35 @@ const CATEGORIES = [
 ];
 
 export function CategoryFilter() {
+  const {filters, setCategory, searchRecipes} = useSearchStore();
+
+  const handleCategoryClick = (category: string) => {
+    const newValue = category === "All" ? null: category;
+
+    setCategory(newValue);
+    searchRecipes();
+  }
   return (
     <div className="flex gap-3 px-4 pb-4 pt-2 overflow-x-auto no-scrollbar bg-card sticky top-[120px] z-20">
-      {CATEGORIES.map((cat, index) => (
-        <Button
-          key={cat.label}
-          variant={cat.active ? "default" : "outline"}
-          className={`flex h-9 shrink-0 items-center justify-center gap-x-2 rounded-full pl-5 pr-5 shadow-sm transition-colors ${
-            cat.active
-              ? "bg-primary text-primary-foreground hover:bg-primary/90"
-              : "bg-background border-border text-foreground hover:bg-muted"
-          }`}
-        >
-          {cat.icon && <span className="text-lg">{cat.icon}</span>}
-          <p className="text-sm font-bold leading-normal">{cat.label}</p>
-        </Button>
-      ))}
+      {CATEGORIES.map((cat, index) => {
+        const isActive =
+          cat.label === 'All' ? filters.category === null : filters.category === cat.label
+        return (
+          <Button
+            key={cat.label}
+            onClick={() => handleCategoryClick(cat.label)}
+            variant={isActive ? "default" : "outline"}
+            className={`flex h-9 shrink-0 items-center justify-center gap-x-2 rounded-full pl-5 pr-5 shadow-sm transition-colors ${
+              isActive
+                ? "bg-primary text-primary-foreground hover:bg-primary/90"
+                : "bg-background border-border text-foreground hover:bg-muted"
+            }`}
+          >
+            {cat.icon && <span className="text-lg">{cat.icon}</span>}
+            <p className="text-sm font-bold leading-normal">{cat.label}</p>
+          </Button>
+        );
+      })}
     </div>
   );
 }
--- lib/stores/useSearchStore.ts
+++ lib/stores/useSearchStore.ts
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+// import axios from "../api/axios";
+import { MOCK_RECIPES } from "@/lib/mockData";
 
 interface SearchState {
   query: string;
@@ -7,24 +9,71 @@ interface SearchState {
     tags: string[];
   };
   results: Recipe[];
+  isLoading: boolean;
+
+  // Actions
   setQuery: (query: string) => void;
   setCategory: (category: string | null) => void;
   searchRecipes: () => Promise<void>; // Skeleton
 }
 
-export const useSearchStore = create<SearchState>((set) => ({
+export const useSearchStore = create<SearchState>((set, get) => ({
   query: "",
   filters: {
     category: null,
     tags: [],
   },
-  results: [],
+  // results: [],
+  results: MOCK_RECIPES,
+  isLoading: false,
+
   setQuery: (query) => set({ query }),
   setCategory: (category) =>
     set((state) => ({
       filters: { ...state.filters, category },
     })),
+
   searchRecipes: async () => {
     /* To be implemented by Mayor-king */
+    set({isLoading: true}); 
+
+    setTimeout(() => {
+      const {query, filters} = get()
+      let filtered = MOCK_RECIPES;
+
+      if (query) {
+        filtered = filtered.filter(recipe => 
+          recipe.title.toLowerCase().includes(query.toLowerCase())
+        )
+      }
+
+      if (filters.category && filters.category !== "All") {}
+
+      set({results: filtered, isLoading: false});
+    }, 500)
+    // const {query, filters} = get();
+
+    // set({isLoading: true});
+
+    // try {
+    //   // 3. Prepare the URL parameters
+    //   // Result: /search/recipes?q=pasta&category=dinner&tags=easy,quick
+    //   const params = new URLSearchParams();
+    //   if (query) params.append("q", query);
+    //   if (filters.category) params.append("category", filters.category);
+    //   filters.tags.forEach(tag => params.append("tags", tag));
+
+    //   // 4. Make the API call
+    //   const response = await axios.get(`/search/recipes?${params.toString()}`)
+
+    //   set({results: response.data, isLoading: false});
+
+    // } catch (error) {
+    //   console.error("Error failed:", error);
+
+    //   set({results: [], isLoading: false})
+    // }
+
+
   },
 }));
